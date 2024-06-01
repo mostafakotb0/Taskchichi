2fe08315d2acb7a8f74430e51936d4f535d98c00
mostafakotb0 <mostafakotb045@gmail.com>
2024-06-01 19:06:39 +0300

    Added Loop Task

---
+++ Task_loops.py
@@ -0,0 +1,10 @@
+# q1
+numbers = [12,75,150,180,145,525,50]
+
+for num in numbers:
+    if num>500:
+        break
+    if num%5==0:
+        if num >150:
+            continue
+        print(num)    
